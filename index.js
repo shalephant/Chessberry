8ff1d50b8a1c3b612270c9bb658b5e7fa522003d
Shako <shakotsiklauri@gmail.com>
2022-07-04 22:50:48 +0400

    adjusted first page image

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const date_of_birth = document.getElementById('date_of_birth');
 
-form.addEventListener('next', (e) => {
+form.addEventListener('submit', e => {
 e.preventDefault();
 
 checkInputs();
-})
+});
 
 function checkInputs() {
-    const ameValue = username.value;
-    const emailValue = email.value;
-    const phone = Valuephone.value;
-    const dateValue = date_of_birth.value;
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const phoneValue = phone.value.trim();
+    const dateValue = date_of_birth.value.trim();
 
     if(usernameValue === '') {
         // show error 
@@ -27,9 +27,11 @@ function checkInputs() {
 }
 
 function setErrorFor(input) {
-    const info = input.parentElement; //.info
-    const div = info.querySelector('div');
+    const inputControl = element.parentElement; //.input-control
+    const errorDisplay = inputControl.querySelector('.error');
 
     //add error class
-    info.className = 'info error';
+   
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
 }
